4ace5722bb081bbd6b99a3eaf3a0d941adc7cae2
hageti <hageti@hispeed.ch>
2021-07-09 09:25:05 +0200

    changes 9 jul

--- code.R
+++ code.R
@@ -1,5 +1,13 @@
-library(tidyverse)
-
+library(tidyverse)  # includes readr
+library(dslabs)
+library(readxl)
+library(dslabs)
+library(purrr)
+library("pdftools")
+library(lubridate)
+library(textdata)
+library(tidytext)
+library(gutenbergr)
 
 
 # what is this? 
